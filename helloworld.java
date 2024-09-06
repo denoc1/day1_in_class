b5afe5c5d4d32e9782d898f0a99bb779b96788a6
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-09-06 13:03:16 +0000

    Fixed issue

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,7 @@
 public class helloworld{
-    public class main (String[] args){
+    public static void main (String[] args){
 
         //write your code after this line but before the "}"
-
-        
-System.out.println("hello world");
 
     }
 
