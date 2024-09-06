9da265978134c2e3b64d571769659b86fdc8141b
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-09-06 01:03:42 +0000

    include code interface

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld{
+    public class main (String[] args){
+
+        //write your code after this line but before the "}"
+
+        
+System.out.println("hello world");
+
+    }
+
+
+}
