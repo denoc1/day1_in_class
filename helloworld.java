e62a4169d694ba1cf11b860578502c6df851d59c
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-09-08 20:09:08 -0400

    Testing

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class helloworld{
     public static void main (String[] args){
 
         //write your code after this line but before the "}"
-
+System.out.println("Hello world!");
     }
 
 
